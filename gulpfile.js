3031bfbb250dcf71dd51d0f71499de953ea874e1
td <tracy.dyer@lrs.com>
2016-05-17 16:47:26 -0500

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+﻿var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    notify = require("gulp-notify"),
+    bower = require('gulp-bower');
+
+var config = {
+    sassPath: './lib/src/sass',
+    bowerDir: './bower_components'
+}
+
+gulp.task('bower', function() {
+    return bower()
+        .pipe(gulp.dest(config.bowerDir))
+});
+
+gulp.task('icons', function() {
+    return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*')
+        .pipe(gulp.dest('./lib/dest/fonts'));
+});
+
+gulp.task('js', function() {
+    return gulp.src(config.bowerDir + '/bootstrap-sass-official/assets/javascripts/bootstrap.min.js')
+        .pipe(gulp.dest('./lib/dest/js'));
+});
+
+gulp.task('css', function() {
+    sass('./lib/src/sass/**/*.scss', {
+        style: 'compressed',
+        loadPath: [
+            config.bowerDir + '/bootstrap-sass-official/assets/stylesheets',
+            config.bowerDir + '/fontawesome/scss'
+        ]
+    })
+    .on('error', sass.logError)
+    .pipe(gulp.dest('./lib/dest/css'));
+});
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+    gulp.watch(config.sassPath + '/**/*.scss', ['css']);
+    gulp.watch(config.bowerDir + '/bootstrap-sass-official/assets/stylesheets/**/*.scss', ['css']);
+});
+
+gulp.task('default', ['bower', 'icons', 'js', 'css', 'watch']);
